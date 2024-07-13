184cb856db562a2118f74aa4298f60103748d005
Mauricio Mitolo <mauricio.estebanmitolo@hashicorp.com>
2024-07-13 21:14:38 +0200

    keep adjusting variables

--- compute.tf
+++ compute.tf
@@ -12,8 +12,8 @@ locals {
       subnetwork           = module.acme_vpc.subnets["us-central1/pub-subnet"].id
       name_prefix          = "${var.default_labels.owner}-vm-front"
       machine_type         = "e2-standard-4"
-      source_image_project = "acme-corp-gcp"
-      source_image         = data.hcp_packer_artifact.gcp_ubuntu_acme_frontend_img.external_identifier
+      #source_image_project = "acme-corp-gcp"
+      #source_image         = data.hcp_packer_artifact.gcp_ubuntu_acme_frontend_img.external_identifier
     }
   }
 }
