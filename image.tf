1003ea3b17c825b9a20671122998412ec959dd24
Mauricio Mitolo <mauricio.estebanmitolo@hashicorp.com>
2024-07-14 20:39:41 +0200

    minor changes 2

--- image.tf
+++ image.tf
@@ -7,5 +7,5 @@ data "hcp_packer_artifact" "gcp_ubuntu_acme_frontend_img" {
   bucket_name         = data.hcp_packer_version.ubuntu_acme_frontend.bucket_name
   version_fingerprint = data.hcp_packer_version.ubuntu_acme_frontend.fingerprint
   platform            = "gce"
-  region              = var.gcp_region
+  region              = "us-central1-a"
 }
