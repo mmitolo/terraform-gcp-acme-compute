6bca07e92524ab443f0b5c82be0490a13cdd5735
Mauricio Mitolo <mauricio.estebanmitolo@hashicorp.com>
2024-07-17 09:02:31 +0200

    add instance output data

--- output.tf
+++ output.tf
@@ -1,7 +1,11 @@
-#output "ubuntu_acme_frontend" {
-#  value = data.hcp_packer_version.ubuntu_acme_frontend
-#}
-#
-#output "gcp_ubuntu_acme_frontend_img" {
-#  value = data.hcp_packer_artifact.gcp_ubuntu_acme_frontend_img
-#}
+output "ubuntu_acme_frontend" {
+  value = data.hcp_packer_version.ubuntu_acme_frontend
+}
+
+output "gcp_ubuntu_acme_frontend_img" {
+  value = data.hcp_packer_artifact.gcp_ubuntu_acme_frontend_img
+}
+
+output "gcp_instance_details" {
+  value = module.compute_instance.instances_details
+}
